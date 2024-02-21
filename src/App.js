1f2a134f22bab29fed9b454e00d1e219d2003188
NinadGawankar <40296976+NinadGawankar@users.noreply.github.com>
2024-02-22 02:28:56 +0530

    Addition of language bar
    Added language bar as same as in bing search engine and it's related styles in App.css file

--- src/App.js
+++ src/App.js
@@ -27,6 +27,20 @@ function App() {
             <img src="   https://cdn-icons-png.flaticon.com/512/11749/11749338.png" alt="Image Search icon" />
           </div>
         </div>
+        <div className="langbar">
+          <span className="languageBarItem">Languages:</span> &nbsp;
+          <a className="languageBarItem" href="/?setlang=hi&amp;cc=in&amp;cc=IN" data-h="ID=HpApp,32432.1" aria-label="Hindi">हिंदी</a> &nbsp;
+          <a className="languageBarItem" href="/?setlang=bn&amp;cc=in&amp;cc=IN" data-h="ID=HpApp,32432.1" aria-label="Bangla">বাংলা</a> &nbsp;
+          <a className="languageBarItem" href="/?setlang=ur&amp;cc=in&amp;cc=IN" data-h="ID=HpApp,32432.1" aria-label="Urdu">اردو</a> &nbsp;
+          <a className="languageBarItem" href="/?setlang=pa-guru&amp;cc=in&amp;cc=IN" data-h="ID=HpApp,32432.1" aria-label="Punjabi (Gurmukhi)">ਪੰਜਾਬੀ</a> &nbsp;
+          <a className="languageBarItem" href="/?setlang=mr&amp;cc=in&amp;cc=IN" data-h="ID=HpApp,32432.1" aria-label="Marathi">मराठी</a> &nbsp;
+          <a className="languageBarItem" href="/?setlang=te&amp;cc=in&amp;cc=IN" data-h="ID=HpApp,32432.1" aria-label="Telugu">తెలుగు</a> &nbsp;
+          <a className="languageBarItem" href="/?setlang=ta&amp;cc=in&amp;cc=IN" data-h="ID=HpApp,32432.1" aria-label="Tamil">தமிழ்</a> &nbsp;
+          <a className="languageBarItem" href="/?setlang=kn&amp;cc=in&amp;cc=IN" data-h="ID=HpApp,32432.1" aria-label="Kannada">ಕನ್ನಡ</a> &nbsp;
+          <a className="languageBarItem" href="/?setlang=gu&amp;cc=in&amp;cc=IN" data-h="ID=HpApp,32432.1" aria-label="Gujarati">ગુજરાતી</a> &nbsp;
+          <a className="languageBarItem" href="/?setlang=ml&amp;cc=in&amp;cc=IN" data-h="ID=HpApp,32432.1" aria-label="Malayalam">മലയാളം</a> &nbsp;
+          <a className="languageBarItem" href="/?setlang=or&amp;cc=in&amp;cc=IN" data-h="ID=HpApp,32432.1" aria-label="Odia">ଓଡ଼ିଆ</a>
+		</div>
       </header>
     </div>
   );
