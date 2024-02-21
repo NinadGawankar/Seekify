2e99f7fb4481dbf4763e9b59ee746c904e9ae18c
NinadGawankar <40296976+NinadGawankar@users.noreply.github.com>
2024-02-22 02:20:19 +0530

    Addition of topnav bar
    Added topnav bar and added its related styles in App.css file. Bootstrap import has been included to use bootstrap classes in file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import logo from './img/logo.png';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
+      <div class="container topnav">
+        <a className="topnav_lowbottom" href="#Copilot"><h4>Seekify</h4></a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="#Copilot">Copilot</a>
+        <a href="#Images">Images</a>
+        <a href="#Videos">Videos</a>
+        <a href="#Maps">Maps</a>
+        <a href="#News">News</a>
+        <a className="topnav_lowtop" href="#More"><h4 className='h2More'>...</h4></a>
+      </div>
       <header className="App-header">
         <img src={logo} alt="logo" />
         <p>
