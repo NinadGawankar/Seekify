33f172650c1e47c4df35241551b2cbe87278c210
NinadGawankar <40296976+NinadGawankar@users.noreply.github.com>
2024-02-22 13:38:36 +0530

    Removal of unnecessary links
    Removal of unnecessary links that are not required and non usable

--- src/Result.js
+++ src/Result.js
@@ -26,12 +26,6 @@ function Result() {
                                 </div>
                             </div>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" href="#">Disabled</a>
-                        </li>
                     </ul>
                 </div>
             </nav>
