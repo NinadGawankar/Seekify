3ed6d25fd5b678f2480cec75c8ed205e1a3cbde0
NinadGawankar <40296976+NinadGawankar@users.noreply.github.com>
2024-02-22 13:36:33 +0530

    Designing of Result component and Addition of Result.css
    Designing of Result component as close as posible to bing and Addition of Result.css. to store all the styles specific to Result component

--- src/Result.js
+++ src/Result.js
@@ -1,10 +1,56 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import './Result.css';
 
 function Result() {
     document.body.style.backgroundImage = "none";
+
     return (
-        <div className="App">
-        </div>
+        <>
+            <nav class="navbar navbar-expand-sm bg-light navbar-light">
+                <div class="container-fluid">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link naming" href="#"><h4>Seekify</h4></a>
+                        </li>
+                        <li class="nav-item">
+                            <div class="search-container-result">
+                                <div class="search-icon">
+                                    <img src="https://cdn-icons-png.flaticon.com/128/54/54481.png" alt="Search icon" />
+                                </div>
+                                <input type="text" placeholder="Search the web" class="search-input" />
+                                <div class="mic-icon">
+                                    <img src="https://cdn-icons-png.flaticon.com/512/13407/13407108.png " alt="Mic icon" />
+                                </div>
+                                <div class="image-search-icon">
+                                    <img src="   https://cdn-icons-png.flaticon.com/512/11749/11749338.png" alt="Image Search icon" />
+                                </div>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#">Disabled</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <div class="topnav-result" id="topnavResult">
+                <a href="#Search">SEARCH</a>
+                <a href="#Copilot">COPILOT</a>
+                <a href="#Images">IMAGES</a>
+                <a href="#Videos">VIDEOS</a>
+                <a href="#Maps">MAPS</a>
+                <a href="#News">NEWS</a>
+                <a href="#Shopping">SHOPPING</a>
+                <a href="#More">MORE</a>
+                <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                    <i class="fa fa-bars"></i>
+                </a>
+            </div>
+            <div className="App">
+            </div>
+        </>
     );
 }
 
